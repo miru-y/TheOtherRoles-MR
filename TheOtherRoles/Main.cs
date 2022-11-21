80bfaad8f7aef0d048b551aedab7ed5f423d7904
miru-y <m_tyc3@yahoo.co.jp>
2022-11-21 23:02:39 +0900

    Version set to v2.5.0.

--- TheOtherRoles/Main.cs
+++ TheOtherRoles/Main.cs
@@ -31,7 +31,7 @@ namespace TheOtherRoles
     {
         public const string Id = "me.eisbison.theotherroles";
 
-        public const string VersionString = "2.4.0";
+        public const string VersionString = "2.5.0";
         public static uint betaDays = 0;  // amount of days for the build to be usable (0 for infinite!)
 
         public static System.Version Version = System.Version.Parse(VersionString);
