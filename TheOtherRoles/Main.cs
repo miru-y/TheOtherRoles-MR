5e290c036928f6a3fbe14de10d060849b562083d
miru-y <m_tyc3@yahoo.co.jp>
2022-12-07 21:51:20 +0900

    Version set to v2.8.0.

--- TheOtherRoles/Main.cs
+++ TheOtherRoles/Main.cs
@@ -35,7 +35,7 @@ namespace TheOtherRoles
     {
         public const string Id = "me.eisbison.theotherroles";
 
-        public const string VersionString = "2.7.1";
+        public const string VersionString = "2.8.0";
         public static uint betaDays = 0;  // amount of days for the build to be usable (0 for infinite!)
 
         public static System.Version Version = System.Version.Parse(VersionString);
